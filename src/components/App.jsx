a1094f9057537495fc10cc2af06f1759315b9b07
VladyslavLata <vladlatahtc@gmail.com>
2023-07-06 14:29:17 +0300

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,8 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 35,
-        color: '#025401',
+        fontSize: 36,
+        color: '#045401',
       }}
     >
       React homework template!
